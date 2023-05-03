a92236ad3eb26b4f7389201ba11c42299710b796
Jignesh Baria <71313366+Jignesh220@users.noreply.github.com>
2023-05-03 17:19:58 +0530

    Footer: replace <a> tag to Next/Link

--- src/components/Reusable/Footer.tsx
+++ src/components/Reusable/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
@@ -158,7 +159,7 @@ export default function Footer() {
 
         <ul className="mt-12 flex justify-center gap-6 md:gap-8">
           <li>
-            <a
+            <Link
               href="/"
               rel="noreferrer"
               target="_blank"
@@ -177,11 +178,11 @@ export default function Footer() {
                   clip-rule="evenodd"
                 />
               </svg>
-            </a>
+            </Link>
           </li>
 
           <li>
-            <a
+            <Link
               href="/"
               rel="noreferrer"
               target="_blank"
@@ -200,11 +201,11 @@ export default function Footer() {
                   clip-rule="evenodd"
                 />
               </svg>
-            </a>
+            </Link>
           </li>
 
           <li>
-            <a
+            <Link
               href="/"
               rel="noreferrer"
               target="_blank"
@@ -219,11 +220,11 @@ export default function Footer() {
               >
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
               </svg>
-            </a>
+            </Link>
           </li>
 
           <li>
-            <a
+            <Link
               href="/"
               rel="noreferrer"
               target="_blank"
@@ -242,63 +243,63 @@ export default function Footer() {
                   clip-rule="evenodd"
                 />
               </svg>
-            </a>
+            </Link>
           </li>
         </ul>
         <nav aria-label="Footer Nav" className="mt-12">
           <ul className="flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
             <li>
-              <a
+              <Link
                 className="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 About
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 className="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 Careers
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 className="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 History
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 className="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 Services
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 className="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 Projects
-              </a>
+              </Link>
             </li>
 
             <li>
-              <a
+              <Link
                 className="text-gray-700 transition hover:text-gray-700/75"
                 href="/"
               >
                 Blog
-              </a>
+              </Link>
             </li>
           </ul>
         </nav>
